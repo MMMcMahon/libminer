51385d9dc0ea3eaf7dbbbdb1318a799058ebc7aa
Mike McMahon <mike.mcmahon@dfo-mpo.gc.ca>
2024-11-04 20:23:03 +0000

    updates readme

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,4 +1,14 @@
+#' Provide Number of R Packages by Library
+#'
+#' Provide the number of R packages by library in a data.frame
+#'
+#' @return a data frame of R packages by library
+#' @export
+#'
+#' @examples
+#' lib_summary()
 lib_summary <- function() {
+
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
